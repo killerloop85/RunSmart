1c55bef0ab5f07944514dd1846d3abbdf7c8548f
Vasilii <killerloop85@gmail.com>
2020-04-16 12:13:11 +0400

    3 Homework
    Slick + adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
                 breakpoint: 992,
                 settings: {
                     dots: true,
-                    arrows: false
+                    arrows: false,
                 }
             }
         ]
-      });
+    });
   });
