f54fb05bf336968bd78c7bcf83b5e57ac2d26647
Vasilii <killerloop85@gmail.com>
2020-04-22 19:42:46 +0400

    7 lesson
    validator + mask

--- src/js/script.js
+++ src/js/script.js
@@ -49,4 +49,37 @@ $(document).ready(function(){
 			$('.overlay, #order').fadeIn('slow');
 		})
 	})
+
+	function valideForms(form){
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					  minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите Ваше имя",
+					minlength: jQuery.validator.format("Введите минимум {0} символа!")
+				  },
+				phone: "Пожалуйста, введите Ваш номер телефона",
+				email: {
+				  required: "Пожалуйста, введите свой email",
+				  email: "Неправильно введен адрес почты"
+				}
+			}
+		});
+	};
+
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-9999");
   });
